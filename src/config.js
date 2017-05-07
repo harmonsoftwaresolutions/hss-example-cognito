4449591cf9dead731fe6f83a279eb79a0d55ee8a
Evan Harmon <evan.harmon@levvel.io>
2017-05-07 13:01:42 -0700

    add editor config add cognito config add basic signup form

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  region: 'us-west-2',
+  IdentityPoolId: 'us-west-2:e0673ca6-0c85-4a15-8944-fa85aa74fe06',
+  UserPoolId: 'us-west-2_JDeVkNfHf',
+  ClientId: '1lejqqpudji4a8f07gh0n03mue'
+}
